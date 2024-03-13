934600e8c60181924fb87d2a8560ee22fec706e0
Preet Dhillon <pdhillon9109@gmail.com>
2024-03-13 17:35:51 -0400

    Final Part

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,4 +1,9 @@
 # Import the dependencies.
+import numpy as np
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 
 
@@ -6,25 +11,134 @@
 # Database Setup
 #################################################
 
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
-
-# Create our session (link) from Python to the DB
-
+Measurement = Base.classes.measurement
+Station =  Base.classes.station
 
 #################################################
 # Flask Setup
 #################################################
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    return(
+        f"Weclome to the Homepage<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= "2016-08-24").all()
+
+    session.close()
+
+
+#Converting List to Dict
+    all_prcp = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = [prcp]
+
+        all_prcp.append(prcp_dict)
+    return jsonify(all_prcp)
+
+@app.route("/api/v1.0/stations")
+def stations():
+  # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    results = session.query(Station.station).order_by(Station.station).all()
+
+    session.close()
+
+#Converting tuple list into list datatype
+    all_stations = list(np.ravel(results))
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    session = Session(engine)
+
+    results = session.query(Measurement.date, Measurement.tobs, Measurement.prcp).filter(Measurement.date >= "2016-08-23").\
+        filter(Measurement.station=='USC00519281').order_by(Measurement.date).all()
+
+    session.close()
+#Converting List to Dict
+    all_tobs = []
+    for prcp, date, tobs in results:
+        tobs_dict = {}
+        tobs_dict["prcp"] = prcp
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+
+        all_tobs.append(tobs_dict)
+
+        return jsonify(all_tobs)
+
+
+
+@app.route("/api/v1.0/<start>")
+def start_date(start_date):
+    #Create session
+    session = Session(engine)
+    
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), \
+                            func.max(Measurement.tobs)).filter(Measurement.date >= start_date).all()
+    session.close()
+
+    #create dict 
+    start_date_tobs = []
+    for min, avg, max in results:
+        start_date_tobs_dict = {}
+        start_date_tobs_dict["min_temp"]= min
+        start_date_tobs_dict["avg_temp"] = avg
+        start_date_tobs_dict["max_temp"] = max
+        start_date_tobs.append(start_date_tobs_dict)
+    return jsonify(start_date_tobs)
+
+       
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start_date, end_date):
+    #Create session
+    session = Session(engine)
+    
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), \
+                            func.max(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    session.close()
+
+    #create dict 
+    start_end_tobs = []
+    for min, avg, max in results:
+        start_end_tobs_dict = {}
+        start_end_tobs_dict["min_temp"]= min
+        start_end_tobs_dict["avg_temp"] = avg
+        start_end_tobs_dict["max_temp"] = max
+        start_end_tobs.append(start_end_tobs_dict)
+    return jsonify(start_end_tobs)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
